7d20e72469a356ff0c6c4c94569e1f31f2b03537
Sergio <shdzvelazco@gmail.com>
2025-08-19 22:28:36 -0600

    Se agrega model de medicamento

---
+++ SaludGestREST.Data/Models/Medicamento.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SaludGestREST.Data.Models
+{
+    public class Medicamento : Registry
+    {
+        public int MedicamentoId { get; set; }
+        public string Nombre { get; set; }
+        public string Sustancia { get; set; }
+        public string Lote { get; set; }
+        public string Codigo { get; set; }
+    }
+}
---
+++ SaludGestREST.Services/Services/Interfaces/IGenericService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaludGestREST.Services.Services.Interfaces
+{
+    public interface IGenericService<TCreateDTO, TReadDTO, TUpdateDTO, TDetails>
+        where TCreateDTO : class
+        where TReadDTO : class
+        where TUpdateDTO : class
+        where TDetails : class
+    {
+        Task<List<TReadDTO>> GetAllAsync();
+        Task<TReadDTO> GetByIdAsync(int id);
+        Task<TDetails> GetDetailsByIdAsync(int id);
+        Task AddAsync(TCreateDTO dto);
+        Task UpdateAsync(int id, TUpdateDTO dto);
+        Task DeleteAsync(int id);
+    }
+}
