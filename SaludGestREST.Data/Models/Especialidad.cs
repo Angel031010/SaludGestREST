aae147c9d6abce32a8edb3e691403a52e3c8c154
tr-net-07-rfz <tr-net-07@DESKTOP-58AU1F9>
2025-08-19 21:50:55 -0600

    feat:se agrega modelo especialidad

---
+++ SaludGestREST.Data/Models/Especialidad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaludGestREST.Data.Models
+{
+    public class Especialidad : Registry
+    {
+        public int IdEspecialidad { get; set; }
+        public string Nombre { get; set; }
+    }
+}
