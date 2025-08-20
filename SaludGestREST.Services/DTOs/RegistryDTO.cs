4a94b8cfb1236aa88eb3f885845a2f6e3810e051
tr-net-07-rfz <tr-net-07@DESKTOP-58AU1F9>
2025-08-19 22:18:24 -0600

    feat: se agrega RegistryDTO

---
+++ SaludGestREST.Services/DTOs/RegistryDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaludGestREST.Services.DTOs
+{
+    public class RegistryDTO
+    {
+        [Display(Name = "Estatus")]
+        public bool Active { get; set; } = true;
+
+        [Display(Name = "Alta")]
+        public DateTime HighSystem { get; set; } = DateTime.Now;
+        [Display(Name = "Actualizado")]
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
